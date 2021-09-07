93b14639198b1195e302e06d5e2fea9da61304bb
Nikhil Sharma <nikhil.harsh.sharma@gmail.com>
2021-09-08 03:05:04 +0530

    feat: added calculating profit, loss, percentage

---
+++ app.js
@@ -0,0 +1,39 @@
+const initialPriceInput = document.querySelector(".initial-price");
+const quantityStockInput = document.querySelector(".quantity-stock");
+const currentPriceInput = document.querySelector(".current-price");
+
+const resultMsg = document.querySelector(".result");
+const errorMsg = document.querySelector(".error");
+
+const submitBtn = document.querySelector(".btn-submit");
+
+submitBtn.addEventListener("click", function handleSubmitClick() {
+  resultMsg.style.display = "none";
+  errorMsg.style.display = "none";
+
+  const initialPrice = Number(initialPriceInput.value);
+  const quantity = Number(quantityStockInput.value);
+  const currentPrice = Number(currentPriceInput.value);
+
+  if (initialPrice && quantity && currentPrice) {
+    const initialAmount = initialPrice * quantity;
+    const currentAmount = currentPrice * quantity;
+
+    if (currentAmount > initialAmount) {
+      const profit = currentAmount - initialAmount;
+      const percentage = (profit / initialAmount) * 100;
+      resultMsg.style.display = "block";
+      resultMsg.style.color = "green";
+      resultMsg.innerText = `This Stock Is In PROFIT of ${profit}, ${percentage}% ⬆`;
+    } else {
+      const loss = initialAmount - currentAmount;
+      const percentage = (loss / initialAmount) * 100;
+      resultMsg.style.display = "block";
+      resultMsg.style.color = "red";
+      resultMsg.innerText = `This Stock Is In LOSS of ${loss}, ${percentage}% ⬇`;
+    }
+  } else {
+    errorMsg.style.display = "block";
+    errorMsg.innerText = "Please Enter All The Fields";
+  }
+});
