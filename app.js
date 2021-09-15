4eee36d2e28dc8cdf90394b645f617a0e7b44262
Nikhil Sharma <nikhil.harsh.sharma@gmail.com>
2021-09-16 02:29:51 +0530

    styles: added responsiveness for mobile screen

--- app.js
+++ app.js
@@ -16,29 +16,34 @@ submitBtn.addEventListener("click", function handleSubmitClick() {
   const currentPrice = Number(currentPriceInput.value);
 
   if (initialPrice && quantity && currentPrice) {
-    const initialAmount = initialPrice * quantity;
-    const currentAmount = currentPrice * quantity;
+    if (initialPrice > 0 && quantity > 0 && currentPrice > 0) {
+      const initialAmount = initialPrice * quantity;
+      const currentAmount = currentPrice * quantity;
 
-    if (currentAmount > initialAmount) {
-      const profit = currentAmount - initialAmount;
-      const percentage = (profit / initialAmount) * 100;
-      resultMsg.style.display = "block";
-      resultMsg.style.color = "green";
-      resultMsg.innerText = `This Stock Is In PROFIT of  ₹${profit}, ${percentage.toFixed(
-        2
-      )}% ⬆`;
-    } else if (currentAmount == initialAmount) {
-      resultMsg.style.display = "block";
-      resultMsg.style.color = "black";
-      resultMsg.innerText = "This Stock Is Neither In Profit Nor In Loss ";
+      if (currentAmount > initialAmount) {
+        const profit = currentAmount - initialAmount;
+        const percentage = (profit / initialAmount) * 100;
+        resultMsg.style.display = "block";
+        resultMsg.style.color = "green";
+        resultMsg.innerText = `This Stock Is In PROFIT of  ₹${profit}, ${percentage.toFixed(
+          2
+        )}% ⬆`;
+      } else if (currentAmount == initialAmount) {
+        resultMsg.style.display = "block";
+        resultMsg.style.color = "black";
+        resultMsg.innerText = "This Stock Is Neither In Profit Nor In Loss ";
+      } else {
+        const loss = initialAmount - currentAmount;
+        const percentage = (loss / initialAmount) * 100;
+        resultMsg.style.display = "block";
+        resultMsg.style.color = "red";
+        resultMsg.innerText = `This Stock Is In LOSS of  ₹${loss}, ${percentage.toFixed(
+          2
+        )}% ⬇`;
+      }
     } else {
-      const loss = initialAmount - currentAmount;
-      const percentage = (loss / initialAmount) * 100;
-      resultMsg.style.display = "block";
-      resultMsg.style.color = "red";
-      resultMsg.innerText = `This Stock Is In LOSS of  ₹${loss}, ${percentage.toFixed(
-        2
-      )}% ⬇`;
+      errorMsg.style.display = "block";
+      errorMsg.innerText = "Please Enter Positive Values Only";
     }
   } else {
     errorMsg.style.display = "block";
